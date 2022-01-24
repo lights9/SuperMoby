81054713445385dfe016e2f4b6c87a59c148629f
lights9 <nursarhan@gmail.com>
2022-01-24 06:51:51 +0100

    Added menu and updated level code in main

--- SuperMoby/main.py
+++ SuperMoby/main.py
@@ -1,5 +1,7 @@
 import os
 from sys import exit
+from os import path
+import pickle
 
 import pygame
 
@@ -9,30 +11,39 @@ fps = 60  # framepersec
 
 SCREENHEIGHT = 700
 SCREENWIDTH = 700  # 640, 480
+
+# define variables
 tile_size = 50
 game_over = 0
+main_menu = True
+level = 0
+max_levels = 2
+
 screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-# screen.fill((100, 201, 207))  # 64C9CF
+screen.fill((100, 201, 207))  # 64C9CF
 pygame.display.set_caption('Super Moby')
-background = pygame.image.load(os.path.join('backgrounds', 'probe.jpg'))
+
+# load images
+#background = pygame.image.load(os.path.join('backgrounds', 'probe.jpg'))
 restart_img = pygame.image.load(os.path.join('assets', 'restart_btn.png'))
+start_img = pygame.image.load('assets/start_btn.png')
+exit_img = pygame.image.load('assets/exit_btn.png')
 
 
-# bgX = 0  # keep track of 2 different images at certain screen to never let screen turn blank
-# bgX2 = background.get_width()
+# function to reset level
+def reset_level(level):
+    player.reset(100, SCREENHEIGHT - 130)
+    dino_group.empty()
+    flag_group.empty()
 
+    # load in level data and create world
+    if path.exists(f'level{level}_data'):
+        pickle_in = open(f'level{level}_data', 'rb')
+        world_data = pickle.load(pickle_in)
+    world = World(world_data)
 
-# Zeynep Character Animation & Sprites AFTER CODE with pygame.display.set_caption
-# images for walking left/right, staying still and background
-# load player image
-# walkRight = [pygame.image.load(os.path.join('mobyCha', 'r1.png')), pygame.image.load(os.path.join('mobyCha', 'r2.png')),
-#              pygame.image.load(os.path.join('mobyCha', 'r3.png')), pygame.image.load(os.path.join('mobyCha', 'r4.png')),
-#              pygame.image.load(os.path.join('mobyCha', 'r5.png')), pygame.image.load(os.path.join('mobyCha', 'r6.png'))]
-# walkLeft = [pygame.image.load(os.path.join('mobyCha', 'l1.png')), pygame.image.load(os.path.join('mobyCha', 'l2.png')),
-#             pygame.image.load(os.path.join('mobyCha', 'l3.png')), pygame.image.load(os.path.join('mobyCha', 'l4.png')),
-#             pygame.image.load(os.path.join('mobyCha', 'l5.png')), pygame.image.load(os.path.join('mobyCha', 'l6.png'))]
-#
-# charMoby = pygame.image.load(os.path.join('mobyCha', 'front.png'))
+    return world
+
 
 class Button():
     def __init__(self, x, y, image):
@@ -137,13 +148,17 @@ class Player():
             if pygame.sprite.spritecollide(self, dino_group, False):
                 game_over = -1
 
+            # check for collision with flag
+            if pygame.sprite.spritecollide(self, flag_group, False):
+                game_over = 1
+
             self.rect.x += dx
             self.rect.y += dy
 
         elif game_over == -1:
             self.image = self.dead_image
             if self.rect.y > 200:
-                self.rect.y -=5
+                self.rect.y -= 5
 
             # if self.rect.bottom > SCREENHEIGHT:
             #     self.rect.bottom = SCREENHEIGHT
@@ -151,93 +166,6 @@ class Player():
 
         screen.blit(self.image, self.rect)
         return game_over
-
-        # def draw(self, screen):
-        #
-        #     if self.walkCount + 1 >= 18:  # 18 because we have 9 sprites(png) for walking left/right, which each will be displayed in 3 frames
-        #         self.walkCount = 0
-        #
-        #     if self.left:
-        #         screen.blit(walkLeft[self.walkCount // 3], (self.x, self.y))  # //3 removes decimal
-        #         self.walkCount += 1
-        #         # scrollBackground(-5,0)
-        #
-        #     elif self.right:
-        #         screen.blit(walkRight[self.walkCount // 3], (self.x, self.y))
-        #         self.walkCount += 1
-        #         # scrollBackground(5, 0)
-        #     else:
-        #         screen.blit(charMoby, (self.x, self.y),)
-        #         #pygame.draw.rect(screen, (255, 0, 0), (self.x+10, self.y+5, self.width, self.height), 2)
-
-        # for tile in world.tile_list:
-        #     #y collison
-        #     if tile[1].colliderect(self.rect.x, self.rect.y + dy)
-
-        #pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
-
-    # Zeynep Character Animation & Sprites AFTER CODE with jumpCount = 10
-
-    # class enemy
-    # class Enemy(object):
-    #     walkRightEnemy = [pygame.image.load(os.path.join('enemy', 'r1-e.png')),
-    #                       pygame.image.load(os.path.join('enemy', 'r2-e.png')),
-    #                       pygame.image.load(os.path.join('enemy', 'r3-e.png')),
-    #                       pygame.image.load(os.path.join('enemy', 'r4-e.png')),
-    #                       pygame.image.load(os.path.join('enemy', 'r5-e.png')),
-    #                       pygame.image.load(os.path.join('enemy', 'r6-e.png'))]
-    #     walkLeftEnemy = [pygame.image.load(os.path.join('enemy', 'l1-e.png')),
-    #                      pygame.image.load(os.path.join('enemy', 'l2-e.png')),
-    #                      pygame.image.load(os.path.join('enemy', 'l3-e.png')),
-    #                      pygame.image.load(os.path.join('enemy', 'l4-e.png')),
-    #                      pygame.image.load(os.path.join('enemy', 'l5-e.png')),
-    #                      pygame.image.load(os.path.join('enemy', 'l6-e.png'))]
-    #
-    #     def __init__(self, x, y, width, height, end):
-    #         self.x = x
-    #         self.y = y
-    #         self.width = width
-    #         self.height = height
-    #         self.end = end
-    #         self.path = [self.x, self.end]
-    #         self.walkCount = 0
-    #         self.vel = 3
-    #
-    #     def draw(self, win):
-    #         self.move()
-    #         if self.walkCount + 1 >= 18:
-    #             self.walkCount = 0
-    #         if self.vel > 0:
-    #             win.blit(self.walkRightEnemy[self.walkCount //3], (self.x, self.y))
-    #             self.walkCount += 1
-    #         else:
-    #             win.blit(self.walkLeftEnemy[self.walkCount //3], (self.x, self.y))
-    #             self.walkCount += 1
-    #
-    #     def move(self):
-    #
-    #         if self.vel > 0:
-    #             if self.x + self.vel < self.path[1]:
-    #                 self.x += self.vel
-    # #                self.rect.x += self.vel
-    #             else:
-    #                 self.vel = self.vel * -1
-    #                 self.walkCount = 0
-    #         else:
-    #             if self.x - self.vel > self.path[0]:
-    #                 self.x += self.vel
-    # #                self.rect.x += self.vel
-    #             else:
-    #                 self.vel = self.vel * -1
-    #                 self.walkCount = 0
-
-    # def redraw_game_window():  # window
-    #     screen.blit(background, (0, 0))
-    #     screen.blit(background, (bgX, 0))
-    #     screen.blit(background, (bgX2, 0))
-    #     moby.draw(screen)
-    #     dino.draw(screen)
-    #     pygame.display.update()
 
     def reset(self, x, y):
         self.images_right = []
@@ -300,13 +228,16 @@ class World():
                     dino = Enemy(col_count * tile_size, row_count * tile_size + 15)
                     # groups = .add
                     dino_group.add(dino)
+                if tile == 6:
+                    flag = Flag(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+                    flag_group.add(flag)
                 col_count += 1
             row_count += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
-            #pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+            # pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 class Enemy(pygame.sprite.Sprite):  # want enemy class to be a child of the Sprite class (Sprite has some functions)
@@ -331,30 +262,48 @@ class Enemy(pygame.sprite.Sprite):  # want enemy class to be a child of the Spri
             self.move_counter *= -1
 
 
-world_data = [
-    [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
-    [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 6, 0],
-    [1, -1, -1, -1, -1, -1, 2, 0, 1, 0, -1, -1, 0, 1],
-    [1, 0, -1, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2],
-    [0, -1, 0, 0, 2, -1, -1, -1, -1, -1, -1, -1, -1, 0],
-    [0, 2, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, 0],
-    [0, -1, -1, -1, -1, -1, -1, 2, 0, 1, 2, -1, -1, 0],
-    [0, -1, -1, -1, -1, -1, -1, -1, 2, 0, -1, -1, -1, 2],
-    [0, -1, -1, -1, -1, -1, -1, -1, -1, 4, 5, -1, 1, 0],
-    [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0],
-    [1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 0, 0, 0],
-    [0, -1, -1, -1, -1, 0, 1, -1, -1, -1, -1, -1, -1, 0],
-    [0, -1, -1, -1, 0, 0, 1, -1, -1, -1, -1, -1, 3, 1],
-    [0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-]
+# world_data = [
+#     [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1],
+#     [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 6, 0],
+#     [1, -1, -1, -1, -1, -1, 2, 0, 1, 0, -1, -1, 0, 1],
+#     [1, 0, -1, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+#     [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2],
+#     [0, -1, 0, 0, 2, -1, -1, -1, -1, -1, -1, -1, -1, 0],
+#     [0, 2, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, 0],
+#     [0, -1, -1, -1, -1, -1, -1, 2, 0, 1, 2, -1, -1, 0],
+#     [0, -1, -1, -1, -1, -1, -1, -1, 2, 0, -1, -1, -1, 2],
+#     [0, -1, -1, -1, -1, -1, -1, -1, -1, 4, 5, -1, 1, 0],
+#     [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0],
+#     [1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 0, 0, 0],
+#     [0, -1, -1, -1, -1, 0, 1, -1, -1, -1, -1, -1, -1, 0],
+#     [0, -1, -1, -1, 0, 0, 1, -1, -1, -1, -1, -1, 3, 1],
+#     [0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+# ]
+
+class Flag(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('objects2/6.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 
 player = Player(100, SCREENHEIGHT - 130)
 dino_group = pygame.sprite.Group()
+flag_group = pygame.sprite.Group()
+
+# load in level data and create world
+if path.exists(f'level{level}_data'):
+    pickle_in = open(f'level{level}_data', 'rb')
+    world_data = pickle.load(pickle_in)
 world = World(world_data)
 
 # create buttons
 restart_btn = Button(SCREENWIDTH // 2 - 50, SCREENHEIGHT // 2 + 100, restart_img)
+start_button = Button(SCREENWIDTH // 2 - 350, SCREENHEIGHT // 2, start_img)
+exit_button = Button(SCREENWIDTH // 2 + 150, SCREENHEIGHT // 2, exit_img)
 
 # main loop
 # moby = Player(40, 585, 40, 60)
@@ -363,85 +312,54 @@ restart_btn = Button(SCREENWIDTH // 2 - 50, SCREENHEIGHT // 2 + 100, restart_img
 run = True
 while run:
     clock.tick(fps)  # set FBS to 27
-    # redraw_game_window()
-    # bgX -= 1.4
-    # bgX2 -= 1.4
-    # if bgX < background.get_width() * -1:
-    #     bgX = background.get_width()
-    #
-    # if bgX2 < background.get_width() * -1:
-    #     bgX2 = background.get_width()
-    #
-    # for event in pygame.event.get():  # gets list of all events that happen
-    #     if event.type == pygame.QUIT:
-    #         run = False
 
-    # basics for moving
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT] and moby.x > moby.vel:
-    #     dx -= moby.vel
-    #     moby.x -= moby.vel
-    #     moby.left = True
-    #     moby.right = False
-    # elif keys[pygame.K_RIGHT] and moby.x < 1000 - moby.width - moby.vel:
-    #     dx += moby.vel
-    #     moby.x += moby.vel
-    #     moby.right = True
-    #     moby.left = False
-    # else:
-    #     moby.right = False
-    #     moby.left = False
-    #     moby.walkCount = 0
-    #
-    # if not moby.isJumping:
-    #     # if keys[pygame.K_UP] and y > vel:
-    #     #    y -= vel
-    #     # if keys[pygame.K_DOWN] and y < 800 - height - vel:
-    #     #    y += vel
-    #     if keys[pygame.K_SPACE]:
-    #         moby.isJumping = True
-    #         moby.right = False
-    #         moby.left = False
-    #         moby.walkCount = 0
-    # else:
-    #     if moby.jumpCount >= -7:
-    #         neg = 1
-    #         if moby.jumpCount < 0:  # negative number
-    #             neg = -1
-    #         moby.y -= (moby.jumpCount ** 2) * 0.5 * neg
-    #         moby.jumpCount -= 1
-    #     else:
-    #         moby.isJumping = False
-    #         moby.jumpCount = 7
+    screen.blit(screen, (0, 0))
+    if main_menu == True:
+        if exit_button.draw():
+            run = False
+        if start_button.draw():
+            main_menu = False
+    else:
+        world.draw()
 
-    screen.blit(background, (0, 0))
-    world.draw()
+        if game_over == 0:
+            dino_group.update()
 
-    if game_over == 0:
-        dino_group.update()
+        # works because draw methode is in sprite class included
+        dino_group.draw(screen)
+        flag_group.draw(screen)
 
-    # works because draw methode is in sprite class included
-    dino_group.draw(screen)
+        game_over = player.update(game_over)
 
-    game_over = player.update(game_over)
+        # if player has died
+        if game_over == -1:
+            if restart_btn.draw():
+                world_data = []
+                world = reset_level(level)
+                game_over = 0
 
-    # if player has died
-    if game_over == -1:
-        if restart_btn.draw():
-            player.reset(100, SCREENHEIGHT - 130)
-            game_over = 0
+        # if player has completed the level
+        if game_over == 1:
+            # reset game and go to next level
+            level += 1
+            if level <= max_levels:
+                # reset level
+                world_data = []
+                world = reset_level(level)
+                game_over = 0
+            else:
+                if restart_btn.draw():
+                    level = 1
+                    # reset level
+                    world_data = []
+                    world = reset_level(level)
+                    game_over = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
-
-    # draw our character
-    # draw all our elements
-    # update everything
-    # (made in the above lines, Zeynep)  FramePerSec.tick(FPS)
-    # tells pygame that this while loop should not run faster than 60times per second
 
 pygame.quit()
 exit()
